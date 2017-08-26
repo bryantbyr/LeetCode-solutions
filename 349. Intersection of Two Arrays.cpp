d821d79aa647a9f5482685644605691cf006024f
BRYANT_BYR <13020568972@163.com>
2017-08-26 18:49:53 +0800

    Add files via upload
    tag: Two Pointers

--- 349. Intersection of Two Arrays.cpp
+++ 349. Intersection of Two Arrays.cpp
@@ -15,7 +15,28 @@ public:
         vector<int> r;
         sort(nums1.begin(), nums1.end());
         sort(nums2.begin(), nums2.end());
-        while (m < nums1.size() && n < nums2.size()) {
+        // while (m < nums1.size() && n < nums2.size()) {
+        //     while (nums1[m] < nums2[n]) {
+        //         m++;
+        //         if (m >= nums1.size())
+        //             return r;
+        //     }
+        //     while (nums1[m] > nums2[n]) {
+        //         n++;
+        //         if (n >= nums2.size())
+        //             return r;
+        //     }
+        //     if (nums1[m] == nums2[n] && (r.size() == 0 || r.back() != nums1[m]))
+        //         r.push_back(nums1[m]);
+        //     if(nums1[m] == nums2[n]){
+        //         m++;
+        //         n++;
+        //     }
+        // }
+        // return r;
+        if(nums1.size()==0||nums2.size()==0)
+            return {};
+        while (1) {
             while (nums1[m] < nums2[n]) {
                 m++;
                 if (m >= nums1.size())
@@ -26,22 +47,21 @@ public:
                 if (n >= nums2.size())
                     return r;
             }
-            if (nums1[m] == nums2[n] && (r.size() == 0 || r.back() != nums1[m]))
-                r.push_back(nums1[m]);
-            if(nums1[m] == nums2[n]){
-                m++;
-                n++;
+            if (nums1[m] == nums2[n]) {
+                if (r.size() == 0 || r.back() != nums1[m])
+                    r.push_back(nums1[m]);
+                if (++m >= nums1.size() || ++n >= nums2.size())
+                    return r;
             }
         }
-        return r;
     }
 };
 
 int main()
 {
     Solution S;
-    vector<int> v1 = {1,2, 2, 1};
-    vector<int> v2 = {2,2};
+    vector<int> v1 = {1, 2, 5, 2, 1};
+    vector<int> v2 = {3, 5, 2};
     vector<int> r = S.intersection(v1, v2);
     for (unsigned i = 0; i < r.size(); ++i) {
         cout << r[i] << endl;
