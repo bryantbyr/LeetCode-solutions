49fc25df507993f562193963d357a562a86194c9
BRYANT_BYR <13020568972@163.com>
2017-09-21 23:56:07 +0800

    Add files via upload
    tag: array

--- ContainsDuplicate.java
+++ ContainsDuplicate.java
@@ -6,16 +6,16 @@ import java.util.*;
 //Time:O(nlog(n))
 //Space:O(1)
 //array
-// class Solution {
-//     public boolean containsDuplicate(int[] nums) {
-//         Arrays.sort(nums);
-//         for (int i = 1; i < nums.length ; i++ ) {
-//             if (nums[i - 1] == nums[i])
-//                 return true;
-//         }
-//         return false;
-//     }
-// }
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 1; i < nums.length ; i++ ) {
+            if (nums[i - 1] == nums[i])
+                return true;
+        }
+        return false;
+    }
+}
 
 //Learn from discuss on 20170921
 //Time:O(n)
@@ -42,19 +42,19 @@ class Solution {
 //Time:O(n)
 //Space:O(n)
 //set
-// class Solution {
-//     public boolean containsDuplicate(int[] nums) {
-//         Set<Integer> set = new HashSet<Integer>();
-//         for (int i : nums) {
-//             // if (!set.add(i))
-//             //     return true;
-//             if (set.contains(i))
-//                 return true;
-//             set.add(i);
-//         }
-//         return false;
-//     }
-// }
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<Integer>();
+        for (int i : nums) {
+            // if (!set.add(i))
+            //     return true;
+            if (set.contains(i))
+                return true;
+            set.add(i);
+        }
+        return false;
+    }
+}
 
 public class ContainsDuplicate {
     public static void main(String[] args) {
