6b78cb1199021569b6c937beded9d1f3554498b7
BRYANT_BYR <13020568972@163.com>
2017-10-05 13:44:11 +0800

    Add files via upload
    tag: array*

--- 611. Valid Triangle Number.cpp
+++ 611. Valid Triangle Number.cpp
@@ -90,6 +90,30 @@ public:
 //     }
 // };
 
+//Learn from discuss on 20171005
+//Time:O(n^2)
+//Space:O(1)
+//array
+class Solution {
+public:
+    int triangleNumber(vector<int>& nums) {
+        int res = 0;
+        sort(nums.begin(), nums.end());
+        for (int i = nums.size() - 1; i > 1 ; --i) {
+            int l = 0, r = i - 1;
+            while (l < r) {
+                if (nums[l] + nums[r] > nums[i]) {
+                    res += r - l;
+                    r--;
+                }
+                else
+                    l++;
+            }
+        }
+        return res;
+    }
+};
+
 
 int main()
 {
