420f0b19e1eb7ab103cd0f4b4fe158f4f7be3f5a
Qi <13020568972@163.com>
2018-10-19 18:32:43 +0800

    Add files via upload
    tag: Tree + DP + Recursion

--- 95. Unique Binary Search Trees II.py
+++ 95. Unique Binary Search Trees II.py
@@ -8,7 +8,7 @@ class TreeNode:
         self.right = None
 
 #Learn from solution on 20181018
-#Time:O(n^2)
+#Time:O(2^N)
 #Space:O(n)
 #Tree + Recursion
 class Solution:
@@ -32,7 +32,42 @@ class Solution:
                     res += [node]
         return res
 
+#Learn from solution on 20181019
+#Time:O(2^N)
+#Space:O(N^2)
+#Tree + DP
+class Solution:
+    def generateTrees(self, n):
+        """
+        :type n: int
+        :rtype: List[TreeNode]
+        """
+        if n == 0:
+            return []
+        dp = [[] for i in range(0, n + 1)]
+        dp[0].append(None)
+        for i in range(1, n + 1):
+            for j in range(0, i):
+                for left in dp[j]:
+                    for right in dp[i - (j + 1)]:
+                        node = TreeNode(j + 1)
+                        node.left = left
+                        node.right = self.clone(right, j + 1)
+                        dp[i].append(node)
+        return dp[n]
+
+    def clone(self, node, offset):
+        if not node:
+            return None
+        newNode = TreeNode(node.val + offset)
+        newNode.right = self.clone(node.right, offset)
+        newNode.left = self.clone(node.left, offset)
+        return newNode
+
 
 s = Solution()
-for x in s.generateTrees(3):
-    print(x.val)
+r = s.generateTrees(3)
+print(r)
+for x in r:
+    if x:
+        print(x.val)
