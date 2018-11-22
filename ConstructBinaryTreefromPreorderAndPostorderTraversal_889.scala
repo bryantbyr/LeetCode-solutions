392b9387a008012e89fe4385d26633a7b846ab07
Qi <13020568972@163.com>
2018-11-22 20:25:57 +0800

    redo yesterday's
    tag: Tree + Recursion

--- ConstructBinaryTreefromPreorderAndPostorderTraversal_889.scala
+++ ConstructBinaryTreefromPreorderAndPostorderTraversal_889.scala
@@ -1,25 +1,33 @@
 
-import utils.{TreeNode, TreeUtils}
-import utils.TreeUtils._
+import utils.TreeUtils
+import utils.TreeNode
 
+// Created by bryantbyr on 20181122
+// Time:O(N^2)
+// Space:O(1)
+// Tree + Recursion
 object ConstructBinaryTreefromPreorderAndPostorderTraversal_889 {
-  def constructFromPrePost(pre: Array[Int], post: Array[Int]): utils.TreeNode = {
+  def constructFromPrePost(pre: Array[Int], post: Array[Int]): TreeNode = {
     val len = pre.length
-    helper(pre,post,0,len-1,0,len-1)
+    helper(pre, post, 0, len - 1, 0, len - 1)
   }
 
-  def helper(pre: Array[Int], post: Array[Int],a:Int,b:Int,c:Int,d:Int): utils.TreeNode = {
-    if(a==b)
+  def helper(pre: Array[Int], post: Array[Int], a: Int, b: Int, c: Int, d: Int): TreeNode = {
+    if (a > b || c > d)
       return null
+
     val root = new TreeNode(pre(a))
-    root.left = helper(pre,post,a+1,)
-    root.right = helper(pre,post,)
+    if (a != b) {
+      val partition = post.indexOf(pre(a + 1))
+      root.left = helper(pre, post, a + 1, a + partition - c + 1, c, partition)
+      root.right = helper(pre, post, a + partition - c + 2, b, partition + 1, d - 1)
+    }
 
-
+    return root
   }
 
   def main(args: Array[String]): Unit = {
-    val T = constructFromPrePost(Array(1,2,4,5,3,6,7),Array(4,5,2,6,7,3,1))
+    val T = constructFromPrePost(Array(1, 2, 4, 5, 3, 6, 7), Array(4, 5, 2, 6, 7, 3, 1))
     val treeUtils = new TreeUtils("")
     treeUtils.PrintTree(T)
   }
