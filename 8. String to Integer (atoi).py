0c29e45a316100b991a699c632e97abe6c7479ee
Qi <13020568972@163.com>
2019-02-28 15:39:46 +0800

    Add files via upload
    tag: string

--- 8. String to Integer (atoi).py
+++ 8. String to Integer (atoi).py
@@ -3,58 +3,58 @@
 # @Author  : qirui
 # @FileName: 8. String to Integer (atoi).py
 
-# # string
-# class Solution(object):
-#     def myAtoi(self, str):
-#         """
-#         :type str: str
-#         :rtype: int
-#         """
-#
-#         start, end = -1, -1
-#         for i in range(len(str)):
-#             if start == -1:
-#                 if str[i] in ['+', '-', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
-#                     start = i
-#                 elif str[i] != ' ':
-#                     return 0
-#             elif str[i] not in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
-#                 end = i
-#                 break
-#         if start == -1:
-#             return 0
-#         if end == -1:
-#             end = len(str)
-#
-#         # digits_str = str[start:end]
-#         # ans, base = 0, 1
-#         # for digit in reversed(digits_str):
-#         #     if digit == '-':
-#         #         ans = -ans
-#         #     elif digit != '+':
-#         #         ans += (ord(digit) - ord('0')) * base
-#         #         base *= 10
-#
-#         # ans, base = 0, 1
-#         # for index in range(end - 1, start - 1, -1):
-#         #     if str[index] == '-':
-#         #         ans = -ans
-#         #     elif str[index] != '+':
-#         #         ans += (ord(str[index]) - ord('0')) * base
-#         #         base *= 10
-#
-#         base = 0
-#         sign = -1 if str[start] == '-' else 1
-#         start += (1 if str[start] in ['+', '-'] else 0)
-#         for index in range(start, end):
-#             base = (ord(str[index]) - ord('0')) + base * 10
-#         ans = sign * base
-#
-#         if ans < -2147483648:
-#             ans = -2147483648
-#         elif ans > 2147483647:
-#             ans = 2147483647
-#         return ans
+# string
+class Solution(object):
+    def myAtoi(self, str):
+        """
+        :type str: str
+        :rtype: int
+        """
+
+        start, end = -1, -1
+        for i in range(len(str)):
+            if start == -1:
+                if str[i] in ['+', '-', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
+                    start = i
+                elif str[i] != ' ':
+                    return 0
+            elif str[i] not in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
+                end = i
+                break
+        if start == -1:
+            return 0
+        if end == -1:
+            end = len(str)
+
+        # digits_str = str[start:end]
+        # ans, base = 0, 1
+        # for digit in reversed(digits_str):
+        #     if digit == '-':
+        #         ans = -ans
+        #     elif digit != '+':
+        #         ans += (ord(digit) - ord('0')) * base
+        #         base *= 10
+
+        # ans, base = 0, 1
+        # for index in range(end - 1, start - 1, -1):
+        #     if str[index] == '-':
+        #         ans = -ans
+        #     elif str[index] != '+':
+        #         ans += (ord(str[index]) - ord('0')) * base
+        #         base *= 10
+
+        base = 0
+        sign = -1 if str[start] == '-' else 1
+        start += (1 if str[start] in ['+', '-'] else 0)
+        for index in range(start, end):
+            base = (ord(str[index]) - ord('0')) + base * 10
+        ans = sign * base
+
+        if ans < -2147483648:
+            ans = -2147483648
+        elif ans > 2147483647:
+            ans = 2147483647
+        return ans
 
 # string (once loop)
 class Solution(object):
